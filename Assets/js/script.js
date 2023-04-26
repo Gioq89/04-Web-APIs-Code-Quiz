ebbaab2b4d2cb620f6e723860919da468047badd
giomendes.quezada <gioq89@gmail.com>
2023-04-26 13:53:43 -0700

    added questions and fixed timer

--- Assets/js/script.js
+++ Assets/js/script.js
@@ -4,19 +4,20 @@ var start = document.getElementById("start");
 start.addEventListener("click", startTimer);
 
 function startTimer() {
-  if(!timerStarted) {
-  var timer = document.getElementById("timer");
-  var seconds = 75;
+  if (!timerStarted) {
+    var timer = document.getElementById("timer");
+    var seconds = 75;
 
-  setInterval(function () {
-    seconds--;
-    timer.textContent = seconds + " seconds";
-    
-    if (seconds == 0) {
-      clearInterval();
-    }
-  }, 1000);
-  
-  timerStarted = true;
-}
+    setInterval(function () {
+      seconds--;
+        if (seconds < 0) {
+          clearInterval();
+          timer.textContent = "Time's up!"
+      } else {
+      timer.textContent = seconds + " seconds";
+      }
+      }, 1000);
+
+    timerStarted = true;
+  }
 }
