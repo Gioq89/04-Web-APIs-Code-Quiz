174f8362139a9315f0eaad6b124856389696a076
giomendes.quezada <gioq89@gmail.com>
2023-04-28 00:48:05 -0700

    added screenshot and removed eventlisteners from function

--- Assets/js/script.js
+++ Assets/js/script.js
@@ -2,94 +2,97 @@
 
 var questions = [
   {
-    question: 'Which is NOT a primitive type?',
-    options: ['a. string', 'b. number', 'c. alerts', 'd. boolean'],
-    answer: 'c. alerts',
+    question: "Which is NOT a primitive type?",
+    options: ["a. string", "b. number", "c. alerts", "d. boolean"],
+    answer: "c. alerts",
   },
   {
     question:
-      'The condition in an if / else statement is enclosed within _____.',
+      "The condition in an if / else statement is enclosed within _____.",
     options: [
-      'a. quotes',
-      'b. curly brackets',
-      'c. parentheses',
-      'd. square brackets',
+      "a. quotes",
+      "b. curly brackets",
+      "c. parentheses",
+      "d. square brackets",
     ],
-    answer: 'c. parentheses',
+    answer: "c. parentheses",
   },
   {
-    question: 'Arrays in Javascript can be used to store _____.',
+    question: "Arrays in Javascript can be used to store _____.",
     options: [
-      'a. numbers and strings',
-      'b. other arrays',
-      'c. booleans',
-      'd. all of the above',
+      "a. numbers and strings",
+      "b. other arrays",
+      "c. booleans",
+      "d. all of the above",
     ],
-    answer: 'd. all of the above',
-  },
-  {
-    question:
-      'String values must be enclosed within _____ when being assigned to variables.',
-    options: ['a. comma', 'b. curly brackets', 'c. quotes', 'd. parentheses'],
-    answer: 'c. quotes',
+    answer: "d. all of the above",
   },
   {
     question:
-      'A very useful tool used during development and debugging for printing content to the debugger is:',
+      "String values must be enclosed within _____ when being assigned to variables.",
+    options: ["a. comma", "b. curly brackets", "c. quotes", "d. parentheses"],
+    answer: "c. quotes",
+  },
+  {
+    question:
+      "A very useful tool used during development and debugging for printing content to the debugger is:",
     options: [
-      'a. JavaScript',
-      'b. terminal / bash',
-      'c. for loops',
-      'd. console.log',
+      "a. JavaScript",
+      "b. terminal / bash",
+      "c. for loops",
+      "d. console.log",
     ],
-    answer: 'd. console.log',
+    answer: "d. console.log",
   },
 ];
 
 // creating variables
 var timerStarted = false;
-var start = document.getElementById('start');
+var start = document.getElementById("start");
 var seconds = 75;
 var questionIndex = 0;
 var intervalId;
-var submitArea = document.getElementById('submitArea');
-var result = document.getElementById('result');
-var questionsArea = document.getElementById('questions');
-var highscoreArea = document.getElementById('highscoreArea');
-var correctSound = new Audio('Assets/sounds/correct.wav');
-var incorrectSound = new Audio('Assets/sounds/incorrect.wav');
-var submitButton = document.getElementById('submit');
+var submitArea = document.getElementById("submitArea");
+var result = document.getElementById("result");
+var navbar = document.getElementsByClassName("navbar");
+var questionsArea = document.getElementById("questions");
+var highscoreArea = document.getElementById("highscoreArea");
+var correctSound = new Audio("Assets/sounds/correct.wav");
+var incorrectSound = new Audio("Assets/sounds/incorrect.wav");
+var submitButton = document.getElementById("submit");
 var gobackButton = document.getElementById("goback");
 var clearHighScoresButton = document.getElementById("clear");
-var usersList = localStorage.getItem('users');
+var viewScoresLink = document.getElementById("viewscores");
+var landingArea = document.getElementById("landing-area");
+var usersList = localStorage.getItem("users");
 var usersArray = [];
-var scores = document.getElementById('scores');
+var scores = document.getElementById("scores");
 
 // event listener waiting for start quiz button to be clicked
-start.addEventListener('click', function () {
+start.addEventListener("click", function () {
   startTimer();
   startQuiz();
 });
 
-function showOrHide(element, show) {
+function showEl(element, show) {
   if (show) {
-    element.removeAttribute('class', 'hide');
+    element.removeAttribute("class", "hide");
   } else {
-    element.setAttribute('class', 'hide');
+    element.setAttribute("class", "hide");
   }
 }
 
 // Function to start timer when Button is clicked
 function startTimer() {
   if (!timerStarted) {
-    var timer = document.getElementById('timer');
+    var timer = document.getElementById("timer");
     intervalId = setInterval(function () {
       seconds--;
       if (seconds < 0) {
         clearInterval();
         timer.textContent = "Time's up!";
       } else {
-        timer.textContent = seconds + ' seconds';
+        timer.textContent = seconds + " seconds";
       }
     }, 1000);
 
@@ -99,28 +102,27 @@ function startTimer() {
 
 // Function to have questions appear when Button is clicked
 function startQuiz() {
-  var landingArea = document.getElementById('landing-area');
-  var questionContainer = document.getElementById('questions-container');
+  var questionContainer = document.getElementById("questions-container");
   var currentQuestion = questions[questionIndex];
 
-  showOrHide(landingArea, false);
+  showEl(landingArea, false);
 
   displayQuestion(currentQuestion, questionContainer);
 
   // display the next question once an answer is selected
   function displayQuestion(question, nextQuestion) {
-    nextQuestion.textContent = '';
-    var questionEl = document.createElement('div');
+    nextQuestion.textContent = "";
+    var questionEl = document.createElement("div");
     questionEl.textContent = question.question;
-    questionEl.classList.add('question-text');
+    questionEl.classList.add("question-text");
     nextQuestion.appendChild(questionEl);
 
     for (var i = 0; i < question.options.length; i++) {
-      var optionEl = document.createElement('button');
+      var optionEl = document.createElement("button");
       optionEl.textContent = question.options[i];
       questionContainer.appendChild(optionEl);
 
-      optionEl.addEventListener('click', function () {
+      optionEl.addEventListener("click", function () {
         checkAnswer(this.textContent, question.answer);
       });
     }
@@ -130,13 +132,13 @@ function startQuiz() {
   function checkAnswer(selectedOption, correctAnswer) {
     if (selectedOption === correctAnswer) {
       // answer is correct, move on to the next question
-      result.innerText = 'Correct!';
-      result.setAttribute('class', 'correct result');
+      result.innerText = "Correct!";
+      result.setAttribute("class", "correct result");
       correctSound.play();
     } else {
       // answer is incorrect, deduct 10 seconds from the timer
-      result.innerText = 'Incorrect!';
-      result.setAttribute('class', 'incorrect result');
+      result.innerText = "Incorrect!";
+      result.setAttribute("class", "incorrect result");
       incorrectSound.play();
       seconds -= 10;
     }
@@ -149,27 +151,54 @@ function startQuiz() {
       endQuiz();
     }
   }
-  function displayHighscores() {
-    var parsedUsersList = JSON.parse(usersList);
-    for (var i = 0; i < parsedUsersList.length; i++) {
-      scores.innerHTML += `
-      <div class="userInfoContainer">
-        <div class="userInfo">${parsedUsersList[i].intials}</div>
-        <div class="userInfo">${parsedUsersList[i].score}</div>
-        </div>
-        `;
-    }
-  }
 
   function endQuiz() {
-    showOrHide(questionsArea, false);
-    showOrHide(submitArea, true);
+    showEl(questionsArea, false);
+    showEl(submitArea, true);
     clearInterval(intervalId);
-    displayHighscores();
   }
+}
 
-submitButton.addEventListener('click', function () {
-  var userInitials = document.getElementById('name').value;
+function displayHighscores() {
+  var parsedArray = JSON.parse(usersList);
+  if (usersArray.length) {
+    for (var i = 0; i < usersArray.length; i++) {
+      scores.innerHTML += `
+    <div class="userInfoContainer">
+      <div class="userInfo">${usersArray[i].intials}</div>
+      <div class="userInfo">${usersArray[i].score}</div>
+      </div>
+      `;
+    }
+  } else {
+    for (var i = 0; i < parsedArray.length; i++) {
+      scores.innerHTML += `
+  <div class="userInfoContainer">
+    <div class="userInfo">${parsedArray[i].intials}</div>
+    <div class="userInfo">${parsedArray[i].score}</div>
+    </div>
+    `;
+    }
+  }
+}
+
+gobackButton.addEventListener("click", function () {
+  window.location.reload();
+});
+
+clearHighScoresButton.addEventListener("click", function () {
+  localStorage.clear();
+  scores.innerHTML = null;
+});
+
+viewScoresLink.addEventListener("click", function () {
+  showEl(landingArea, false);
+  showEl(highscoreArea, true);
+  displayHighscores();
+});
+
+submitButton.addEventListener("click", function () {
+  var userInitials = document.getElementById("name").value;
 
   if (usersList) {
     usersArray = JSON.parse(usersList);
@@ -181,17 +210,8 @@ submitButton.addEventListener('click', function () {
   };
 
   usersArray.push(highscoreTable);
-  localStorage.setItem('users', JSON.stringify(usersArray));
-  showOrHide(submitArea, false);
-  showOrHide(highscoreArea, true);
+  localStorage.setItem("users", JSON.stringify(usersArray));
+  showEl(submitArea, false);
+  showEl(highscoreArea, true);
+  displayHighscores();
 });
-
-gobackButton.addEventListener("click", function () {
-  window.location.reload();
-});
-
-clearHighScoresButton.addEventListener("click", function() {
-  localStorage.clear();
-  scores.innerHTML = null;
-});
-}
